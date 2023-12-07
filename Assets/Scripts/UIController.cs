9076319e155a38671103bbd1d9b4dbc9f53334b0
tranvanhoa1296 <Tranvanhoa1296@gmail.com>
2023-12-07 11:58:55 +0700

    Merger_Static_Update1

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    public void OnSpawnButtonClick()
+    {
+        gameManager.SpawnCharacter();
+    }
+
+    public void OnMergeButtonClick()
+    {
+        gameManager.MergeCharacters();
+    }
+}
